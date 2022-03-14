51bae6cfa69698d2fa16aa4d9411225fe3b208da
Toni Hukka <berendin@gmail.com>
2022-03-14 12:42:19 +0200

    portfolio projects array

---
+++ components/portfolio-projects.js
@@ -0,0 +1,26 @@
+export const projectsArr = [
+  {
+    name: "hukka.dev",
+    description: "The page you're currently reading! Build with Next.js. Designed by me.",
+    technology: "JavaScript, NextJS, React",
+    img: "home.svg",
+    imgAlt: "Icon of a house.",
+    url: "https://github.com/thukka/hukka.dev"
+  },
+  {
+    name: "Fitlogger",
+    description: "Full-Stack project for personal use. Multi-user app for logging and displaying bicycling data.",
+    technology: "TypeScript, Node.js, MongoDB, JavaScript, React, Redux, MaterialUI",
+    img: "bicycle.svg",
+    imgAlt: "Icon of a bicycle.",
+    url: "https://github.com/thukka/fitlogger"
+  },
+  {
+    name: "Twitch Automatic Points",
+    description: "Chrome extension for automatically claiming Twitch channel points when logged in.",
+    technology: "JavaScript",
+    img: "chrome.png",
+    imgAlt: "Icon of the Google Chrome logo.",
+    url: "https://github.com/thukka/twitch-automatic-points"
+  },
+];
