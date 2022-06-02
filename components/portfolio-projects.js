87d9ef5289509335de736e111422b168956d77c4
Toni Hukka <berendin@gmail.com>
2022-06-02 13:58:51 +0300

    add latest project

--- components/portfolio-projects.js
+++ components/portfolio-projects.js
@@ -1,13 +1,5 @@
 export const projectsArr = [
   {
-    name: "hukka.dev",
-    description: "The page you're currently reading! Build with Next.js. Designed by me.",
-    technology: "JavaScript, NextJS, React",
-    img: "home.svg",
-    imgAlt: "Icon of a house.",
-    url: "https://github.com/thukka/hukka_dev"
-  },
-  {
     name: "Fitlogger",
     description: "Full-Stack project for personal use. Multi-user app for logging and displaying bicycling data.",
     technology: "TypeScript, Node.js, MongoDB, JavaScript, React, Redux, MaterialUI",
@@ -15,6 +7,23 @@ export const projectsArr = [
     imgAlt: "Icon of a bicycle.",
     url: "https://github.com/thukka/fitlogger"
   },
+  {
+    name: "hukka.dev",
+    description: "The page you're currently reading! Build with Next.js. Designed by me.",
+    technology: "JavaScript, NextJS, React",
+    img: "home.svg",
+    imgAlt: "Icon of a house.",
+    url: "https://github.com/thukka/hukka_dev"
+  },
+  ,{
+    name: "Daily Budget Calculator",
+    description: "Calculate your daily budget for the rest of the month. User input is saved to browser's local storage.",
+    technology: "React, TypeScript",
+    img: "calculator.svg",
+    imgAlt: "Icon of a calculator.",
+    url: "https://github.com/thukka/daily-budget-calc"
+  },
+  ,
   {
     name: "Twitch Automatic Points",
     description: "Chrome extension for automatically claiming Twitch channel points when logged in.",
